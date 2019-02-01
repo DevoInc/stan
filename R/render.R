a439f260e1d30691c11c4a72d7090882a9c983c8
Inés Añibarro <39153852+inesani@users.noreply.github.com>
2019-02-01 10:38:53 +0100

    uploading to opencpu

--- R/render.R
+++ R/render.R
@@ -2,7 +2,7 @@ render <- function(dataframe, dimensions) {
 
   colnames(dataframe) <- dimensions 
 
-  # Alternative source of data for testing
+  # Alternative source of data for testing 
   #dataframe <-read.csv("/usr/local/lib/R/site-library/stan-opencpu/series1.txt", header = TRUE)
 
 	# Render the markdwn
