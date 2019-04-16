defd1e7847ca8883432c7dc13b8d70f9c4038246
Inés Añibarro <39153852+inesani@users.noreply.github.com>
2019-04-16 16:18:38 +0200

    probando probando

--- R/render.R
+++ R/render.R
@@ -1,4 +1,4 @@
-render <- function(dataframe, dimensions) {
+render <- function(dataframe, dimensions, query) {
 
   colnames(dataframe) <- dimensions 
 
@@ -8,6 +8,6 @@ render <- function(dataframe, dimensions) {
 	# Render the markdwn
   rmarkdown::render("/usr/local/lib/opencpu/apps/ocpu_github_devoinc_stan/stan/stan.Rmd", output_file="output.html",
                     output_dir=getwd(),
-                    intermediates_dir=getwd(), params=list(test=dataframe), output_format="html_document");
+                    intermediates_dir=getwd(), params=list(test=dataframe, query=query), output_format="html_document");
   invisible();
 }
